f3d831c74ebb7236b53501dafed005fb88dfa169
Jesiel Viana <jesiel.analista@gmail.com>
2020-04-03 00:21:18 -0300

    add slides and urlPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,20 +104,9 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: 'gatsby-source-filesystem',
-    //   options: {
-    //     path: `${__dirname}/src/slides`,
-    //     plugins: [
-    //       {
-    //         resolve: 'gatsby-plugin-copy-files',
-    //         options: {
-    //           source: `${__dirname}/src/slides`,
-    //           destination: '/slides',
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
+
+    // Optional plugin that enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
   ],
 }
